69450c7bfc1eeec889384a7c71ea67b9000a8c2b
mendozak2atwit <mendozak2@wit.edu>
2025-10-21 11:29:40 -0400

    Added basic imports and server stuff

---
+++ connectFour/src/server/ConnectServer.java
@@ -0,0 +1,15 @@
+package server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class ConnectServer {
+
+	public static void main(String[] args) {
+		int port = 1738;
+		
+	}
+
+}
