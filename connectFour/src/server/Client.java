54ce6877b6bb6d49c2cd70596b1b99f22a5eca3a
Alex Sam <sama2@wit.edu>
2025-10-29 20:59:55 -0400

    the damnation of our souls

--- connectFour/src/server/Client.java
+++ connectFour/src/server/Client.java
@@ -1,3 +1,4 @@
+
 package server;
 
 import java.io.IOException;
@@ -6,35 +7,46 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class Client {
-
-	public static void main(String[] args){
-		String serverHostName = "localhost";
-		int serverPort = 1738;
-		
-		try(DatagramSocket clientSocket = new DatagramSocket()){
-			InetAddress serverAddress = InetAddress.getByName(serverHostName);
-			
-			String message = "hi!";
-			byte[] sendData = message.getBytes();
-			
-			System.out.println("Attemting to connect to host");
-			
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
-			clientSocket.send(sendPacket);
-			System.out.println("Sent to server: " + message);
-
-			byte[] receiveData = new byte[1024];
-			
-			
-		}catch (UnknownHostException e) {
-			System.err.println("Unknown host: " + e.getMessage());
-		}catch (SocketException e) {
-			System.err.println("Socket error: " + e.getMessage());
-		}catch (IOException e) {
-			System.err.println("I/O error: " + e.getMessage());
-		}
-	}
-
-}
+/**
+ * 
+ * @author kmp
+ *
+ * @version 1.0 2025-10-29 Initial implementation
+ *
+ *
+ * @since 1.0
+ */
+public class Client
+    {
+        final int SERVER_PORT = 9154;
+    
+        private static boolean validateIp(String ip) {
+            try {
+                InetAddress.getByName(ip);
+                return true;
+            } catch(UnknownHostException e) {
+                return false;
+            }
+        }
+        
+        public static void main(String[] args) {
+            try (DatagramSocket clientSocket = new DatagramSocket(); Scanner input = new Scanner(System.in)) {
+                String serverName;
+                do {
+                    // message can be changed if you want this is just temp stuff until
+                    System.out.printf("Enter the turi ip ip ip ip ip address: ");
+                    serverName = input.next();
+                    // please let this be a valid ip address
+                } while(!validateIp(serverName));
+                System.out.println(serverName);
+                input.close();
+            } catch (SocketException e) {
+                System.err.println("Socket error: " + e.getMessage());
+            } catch (IOException e) {
+                System.err.println("I/O error: " + e.getMessage());
+            } 
+        }
+    }
+   // end class Client
