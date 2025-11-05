c0f832050e31eed9ed32c5f6c01fe39c8a108be3
mendozak2atwit <mendozak2@wit.edu>
2025-11-05 14:39:08 -0500

    Able to get a few methods working in Board class

--- connectFour/src/connectFour/Test.java
+++ connectFour/src/connectFour/Test.java
@@ -2,9 +2,18 @@ package connectFour;
 
 import java.util.Scanner;
 
+import connectFour.Board.pieceColor;
+
 public class Test {
 	
+	static String playerOneColor = pieceColor.RED.getPiece();
+	static String playerTwoColor = pieceColor.YELLOW.getPiece();
+	
 	public static void main(String[] args) {
+		
+		Board.newBoard();
+		Scanner placeInput = new Scanner(System.in);
+		int place;
 		
 		System.out.println("Player one is Red!");
 		System.out.println("Player two is Yellow!");
@@ -12,7 +21,29 @@ public class Test {
 		System.out.println("This is the current board: ");
 		Board.printBoard();
 		
+		System.out.print("Choose a column (0 - 6): ");
+		place = placeInput.nextInt();
 		
+		if(Board.checkDropPiece(place) == true && Board.checkFullBoard() == false) {
+			Board.dropPiece(playerOneColor, place);
+			System.out.println("passes");
+		}
+		
+		System.out.println("Current Board: ");
+		Board.printBoard();
+		
+		System.out.print("Choose a column (0 - 6): ");
+		place = placeInput.nextInt();
+		
+		if(Board.checkDropPiece(place) == true && Board.checkFullBoard() == false) {
+			Board.dropPiece(playerOneColor, place);
+			System.out.println("passes");
+		}
+		
+		System.out.println("Current Board: ");
+		Board.printBoard();
 	}
+	
+	
 
 }
