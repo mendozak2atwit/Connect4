1b8fa192ba777d255f78e17a1b7ee7d26d181364
mendozak2atwit <mendozak2@wit.edu>
2025-11-05 12:14:20 -0500

    Added ConnectServer and Client

--- connectFour/src/server/Client.java
+++ connectFour/src/server/Client.java
@@ -6,35 +6,57 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
-	public static void main(String[] args){
-		String serverHostName = "localhost";
-		int serverPort = 1738;
-		
-		try(DatagramSocket clientSocket = new DatagramSocket()){
-			InetAddress serverAddress = InetAddress.getByName(serverHostName);
-			
-			String message = "hi!";
-			byte[] sendData = message.getBytes();
-			
-			System.out.println("Attemting to connect to host");
-			
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
-			clientSocket.send(sendPacket);
-			System.out.println("Sent to server: " + message);
+	final static int SERVER_PORT = 9154;
+    
+    private static boolean validateIp(String ip) {
+        try {
+            InetAddress.getByName(ip);
+            return true;
+        } catch(UnknownHostException e) {
+            return false;
+        }
+    }
+    
+    public static void main(String[] args) {
+        try (DatagramSocket clientSocket = new DatagramSocket(); Scanner input = new Scanner(System.in)) {
+            String serverName;
+            do {
+                System.out.printf("Enter the ip address: ");
+                serverName = input.next();
+                // please let this be a valid ip address
+            } while(!validateIp(serverName));
+            System.out.println(serverName);
+            
+            InetAddress serverAddress = InetAddress.getByName(serverName);
+            
+            String message = "Ready to play!";
+            byte[] sendData = message.getBytes();
 
-			byte[] receiveData = new byte[1024];
-			
-			
-		}catch (UnknownHostException e) {
-			System.err.println("Unknown host: " + e.getMessage());
-		}catch (SocketException e) {
-			System.err.println("Socket error: " + e.getMessage());
-		}catch (IOException e) {
-			System.err.println("I/O error: " + e.getMessage());
-		}
-	}
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, SERVER_PORT);
+            clientSocket.send(sendPacket);
+            
+            System.out.println("Sent to server: " + message);
+
+            byte[] receiveData = new byte[1024];
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            //DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length + 1);
+
+            clientSocket.receive(receivePacket); // Blocks until a packet is received
+
+            String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Received from server: " + response);
+            
+            input.close();
+        } catch (SocketException e) {
+            System.err.println("Socket error: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("I/O error: " + e.getMessage());
+        } 
+    }
 
 }
