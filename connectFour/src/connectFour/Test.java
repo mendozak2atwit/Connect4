7595d04d89396a50a31c6a1f0e7f606fde53e038
mendozak2atwit <mendozak2@wit.edu>
2025-11-06 14:19:21 -0500

    Able to get 2/3 of the checkWin working and worked more on testing

--- connectFour/src/connectFour/Test.java
+++ connectFour/src/connectFour/Test.java
@@ -1,49 +1,97 @@
 package connectFour;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import connectFour.Board.pieceColor;
 
 public class Test {
 	
 	static String playerOneColor = pieceColor.RED.getPiece();
 	static String playerTwoColor = pieceColor.YELLOW.getPiece();
+	static boolean columnIntegrity = false;
+	static boolean valid = false;
+	static String playerTurn = playerOneColor;
 	
 	public static void main(String[] args) {
-		
 		Board.newBoard();
-		Scanner placeInput = new Scanner(System.in);
-		int place;
-		
 		System.out.println("Player one is Red!");
 		System.out.println("Player two is Yellow!");
 		
+		while(Board.winner == false) {
+		Scanner placeInput = new Scanner(System.in);
+		int place = 0;
+		
+		System.out.println();
 		System.out.println("This is the current board: ");
+		System.out.printf("Current turn: %s", playerTurn);
 		Board.printBoard();
+			
+		while (!valid) {
+		
+			try {
 		
 		System.out.print("Choose a column (0 - 6): ");
 		place = placeInput.nextInt();
+		valid = true;
 		
-		if(Board.checkDropPiece(place) == true && Board.checkFullBoard() == false) {
-			Board.dropPiece(playerOneColor, place);
-			System.out.println("passes");
+		}catch (InputMismatchException e) {
+			System.out.println("Please use a viable input (0 - 6)");
+			System.out.println();
+			placeInput.next();
+			
 		}
 		
-		System.out.println("Current Board: ");
-		Board.printBoard();
-		
-		System.out.print("Choose a column (0 - 6): ");
-		place = placeInput.nextInt();
-		
-		if(Board.checkDropPiece(place) == true && Board.checkFullBoard() == false) {
-			Board.dropPiece(playerOneColor, place);
-			System.out.println("passes");
 		}
 		
-		System.out.println("Current Board: ");
-		Board.printBoard();
+		while(columnIntegrity == false) {
+		
+		try {
+			Board.checkDropPiece(place);
+		}catch (IndexOutOfBoundsException e) {
+			System.out.println("The number is too large!");
+		}
+			
+		if(Board.checkDropPiece(place) == true && Board.checkFullBoard() == false) {
+			Board.dropPiece(playerTurn, place);
+			System.out.println("passes");
+			columnIntegrity = true;
+			
+		}else {
+			System.out.println("Current column is full! Please choose another one.");
+			columnIntegrity = true;
+			}
+		
+		}
+		
+		Board.checkWinner(playerOneColor);
+		Board.checkWinner(playerTwoColor);
+		
+		if(Board.winner == true) {
+			if(Board.currentTurn == 0) {
+			System.out.println("Red wins!");
+			System.out.println("Winning Board: ");
+			Board.printBoard();
+			System.exit(0);
+			
+		}else {
+			System.out.println("Yellow wins!");
+			System.out.println("Winning Board: ");
+			Board.printBoard();
+			System.exit(0);
+			}
+			
+		}
+		
+		valid = false;
+		columnIntegrity = false;
+		Board.swapTurns();
+		
+		if(Board.currentTurn == 0){
+		playerTurn = playerOneColor;	
+		}else {
+			playerTurn = playerTwoColor;
+		}
+		
+		}
 	}
-	
-	
-
 }
